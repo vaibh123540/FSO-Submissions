aa52c151a6fda12d91798a3ee3a480dce304c5ab
Vaibhav Jain <vaibh123540@Vaibhavs-MacBook-Air.local>
2025-07-06 21:31:47 -0600

    part3 v3

--- Part3/phonebook_backend/index.js
+++ Part3/phonebook_backend/index.js
@@ -1,9 +1,8 @@
 const express = require("express")
 const morgan = require("morgan")
-const cors = require("cors")
 const app = express()
 app.use(express.json())
-app.use(cors())
+app.use(express.static('dist'))
 morgan.token("data", (req, res) => {
     return JSON.stringify(req.body)
 })
